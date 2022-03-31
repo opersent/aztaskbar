830084733096f5fa94a86db65e53c8a843f82793
Andrew Zaech <4151635-AndrewZaech@users.noreply.gitlab.com>
2022-03-31 12:39:51 +0000

    Update windowPreview.js

--- windowPreview.js
+++ windowPreview.js
@@ -372,15 +372,11 @@ class azTaskbar_WindowPreviewMenuItem extends PopupMenu.PopupBaseMenuItem {
 
         let [width, height] = mutterWindow.get_size();
 
-        let scale;
-
-
-            // a simple example with 1680x1050:
-            // * 250/1680 = 0,1488
-            // * 150/1050 = 0,1429
-            // => scale is 0,1429
-            scale = Math.min(1.0, PREVIEW_MAX_WIDTH/width, PREVIEW_MAX_HEIGHT/height)
-
+        // a simple example with 1680x1050:
+        // * 250/1680 = 0,1488
+        // * 150/1050 = 0,1429
+        // => scale is 0,1429
+        let scale = Math.min(1.0, PREVIEW_MAX_WIDTH/width, PREVIEW_MAX_HEIGHT/height)
 
         // width and height that we wanna multiply by scale
         return [width, height, scale];
