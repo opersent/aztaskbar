5b1e2d07d759cc9512c56c441992bd8d0d497525
Andrew Zaech <4151635-AndrewZaech@users.noreply.gitlab.com>
2022-11-30 14:10:14 +0000

    AppDisplayBox: queue redisplay on switch workspace event.

--- extension.js
+++ extension.js
@@ -70,7 +70,7 @@ class azTaskbar_AppDisplayBox extends St.ScrollView {
             AppFavorites.getAppFavorites().reload();
             this._queueRedisplay();
         }), this._appSystem);
-        this._connections.set(global.window_manager.connect('switch-workspace', () => this._connectWorkspaceSignals()), global.window_manager);
+        this._connections.set(global.window_manager.connect('switch-workspace', () => this._queueRedisplay.bind(this)), global.window_manager);
         this._connections.set(global.display.connect('window-entered-monitor', this._queueRedisplay.bind(this)), global.display);
         this._connections.set(global.display.connect('window-left-monitor', this._queueRedisplay.bind(this)), global.display);
         this._connections.set(global.display.connect('restacked', this._queueRedisplay.bind(this)), global.display);
@@ -81,7 +81,6 @@ class azTaskbar_AppDisplayBox extends St.ScrollView {
         //If appDisplayBox position is moved in the main panel, updateIconGeometry
         this.connect("notify::position", () => this._updateIconGeometry());
         this.connect("destroy", () => this._destroy());
-        this._connectWorkspaceSignals();
     }
 
     _createAppItem(newApp, monitorIndex, positionIndex){
@@ -392,23 +391,6 @@ class azTaskbar_AppDisplayBox extends St.ScrollView {
         this.mainBox.queue_relayout();
     }
     
-    _connectWorkspaceSignals() {
-        this._disconnectWorkspaceSignals();
-
-        this._lastWorkspace = global.workspace_manager.get_active_workspace();
-
-        this._workspaceWindowAddedId = this._lastWorkspace.connect('window-added', () => this._queueRedisplay());
-        this._workspaceWindowRemovedId = this._lastWorkspace.connect('window-removed', () => this._queueRedisplay());
-    }
-
-    _disconnectWorkspaceSignals() {
-        if (this._lastWorkspace) {
-            this._lastWorkspace.disconnect(this._workspaceWindowAddedId);
-            this._lastWorkspace.disconnect(this._workspaceWindowRemovedId);
-
-            this._lastWorkspace = null;
-        }
-    }
 
     updateIcon(){
         this.oldAppIcons.forEach((appIcon, appID) => {
@@ -448,7 +430,6 @@ class azTaskbar_AppDisplayBox extends St.ScrollView {
     }
 
     _destroy() {
-        this._disconnectWorkspaceSignals();
         this.removeWindowPreviewCloseTimeout();
 
         this._connections.forEach((object, id) => {
